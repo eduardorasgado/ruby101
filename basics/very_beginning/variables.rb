0841239043c14a939aaf9f4bff60db3d7e9803aa
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-29 04:50:32 -0600

    static class variables

--- basics/very_beginning/variables.rb
+++ basics/very_beginning/variables.rb
@@ -2,16 +2,33 @@
   In here we defined an employee class
 =end
 class Employee
+  @@employee_counter = 0
+
   def initialize(name)
     @name = name
+    # every time class initializes, this value increments for all objects
+    @@employee_counter += 1
   end
 
   def greet
     puts "Hello, my name is: #{@name}"
   end
+
+  def total_of_employees
+    puts @@employee_counter
+  end
 end
 
 name = "Eduardo"
+name2 = "Marcelo"
+name3 = "Cristina"
 em1 = Employee.new(name)
+em2 = Employee.new(name)
+em3 = Employee.new(name)
+
 
 em1.greet
+
+em1.total_of_employees
+em2.total_of_employees
+em3.total_of_employees
