2ad1e078b0a24b2b1c7dfa166067d713fd8540af
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-29 05:14:38 -0600

    completing challenge: numbers in time

---
+++ basics/very_beginning/numbers.rb
@@ -0,0 +1,24 @@
+# prints a float, automatically converts it to a float
+puts 1.0 + 2
+
+puts 3 / 2
+
+puts 3.0 / 2
+
+puts -99
+
+puts -99.abs
+
+puts Math.sqrt(-81.abs).to_int
+
+#challenge
+def playing_with_numbers(hours_in_year, minutes_in_decade, age)
+  age_in_seconds = 0
+  # Write - Your - Code
+
+  hours_in_year = (24 * 31 * 7) + (24 * 30 * 4) + (24 * 28 )
+  minutes_in_decade = hours_in_year * 60 * 10
+  age_in_seconds = hours_in_year * 60 * 60 * age
+
+  return hours_in_year, minutes_in_decade, age_in_seconds
+end
