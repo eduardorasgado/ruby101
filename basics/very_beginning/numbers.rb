95852698f503335c2aa7820f820d57ff898eac04
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-29 05:22:18 -0600

    completing challenge: even or odd and modulus

--- basics/very_beginning/numbers.rb
+++ basics/very_beginning/numbers.rb
@@ -22,3 +22,23 @@ def playing_with_numbers(hours_in_year, minutes_in_decade, age)
 
   return hours_in_year, minutes_in_decade, age_in_seconds
 end
+#modulo challenge
+def calculate_mod(num1, num2)
+  result = -1
+  # Write - Your - Code
+  result = num1 % num2
+
+  return result
+end
+
+# even odd challenge
+def even_or_odd(num)
+  result = ""
+  if num.even?
+    result = "even"
+  else
+    result = "odd"
+  end
+
+  return result
+end
