0f38cf343246fc7e19addcac7a70b01d127fd4ff
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-01 16:57:45 -0600

    Adding the snake game loop and movement

--- basics/very_beginning/snake.rb
+++ basics/very_beginning/snake.rb
@@ -2,6 +2,7 @@ require 'ruby2d'
 
 set title: 'Snake Game'
 set background: 'navy'
+set fps_cap: 5
 
 # height: 480 / 20 = 24
 # width: 640 / 20 = 32
@@ -12,6 +13,7 @@ GRID_SIZE = 20
 class Snake
   def initialize
     @positions = [[2, 0], [2, 1], [2, 2], [2, 3]]
+    @direction = 'down'
   end
 
   # This function will draw the snake every frame
@@ -21,8 +23,34 @@ class Snake
                  size: GRID_SIZE - 1, color: 'white')
     end
   end
+
+  # reading direction that user gives the snake
+  def move
+    # removing first element from the snake body
+    @positions.shift
+    # reading direction
+    case @direction
+    when 'down'
+      # adding one to y in snake head
+      next_head_pos = [head[0], head[1] + 1]
+      @positions.push(next_head_pos)
+    end
+  end
+
+  private
+
+  # returns the snake head position list
+  def head
+    @positions.last
+  end
 end
 
-s = Snake.new
-s.draw
+snake = Snake.new
+
+update do
+  clear
+  snake.move
+  snake.draw
+end
+
 show
